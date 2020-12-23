5b7b882c3167ac136b7dadb3c46d18266a148c52
Tim Bailey <tim.bailey@noknow.nz>
2020-12-23 22:57:58 +1100

    feat: Bundle all the code into a single app feat: First Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM jrottenberg/ffmpeg:4.3-alpine
+FROM node:12-alpine
+
+# copy ffmpeg bins from first image
+COPY --from=0 / /
+
+WORKDIR /var/app
+COPY package*.json yarn.lock ./
+RUN yarn install
+COPY . .
+EXPOSE 8000
+
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-var STREAM_PORT = process.argv[2] || 8081,
-    WEBSOCKET_PORT = process.argv[3] || 8082;
+const Koa = require('koa');
+const websockify = require('koa-websocket');
+const bigInt = require("big-integer");
 
-var WebSocket = require('ws');
-var net = require('net');
-var bigInt = require("big-integer");
-const { send } = require('process');
+const WebSocketOpen = 1
+
+const app = websockify(new Koa(), {perMessageDeflate: false});
+
+app.use(require('koa-static')('public'));
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.ws.use(function(ctx, next) {
+    ctx.websocket.sentHeaders = new Set()
+
+    Object.getOwnPropertyNames(headers).forEach(id => {
+        sendFTYPMOOV(id, socket)
+    })
+})
+
+app.listen(8000);
 
 const MP4_FTYP = 1718909296;
 const MP4_MOOV = 1836019574;
 
 const headers = {
-}
-// var ftyp;
-// var moov;
-
-if (process.argv.length < 3) {
-    console.log('Usage: node ws.js [<stream-port> <websocket-port>]');
-    process.exit();
 }
 
 function binStringToHex2(s) {
@@ -73,28 +82,6 @@ function initFragment(camera, buffer) {
     parseMP4(camera, dataView, 0, buffer.byteLength);
 }
 
-// Websocket Server
-var socketServer = new WebSocket.Server({port: WEBSOCKET_PORT, perMessageDeflate: false});
-
-socketServer.on('connection', function(socket) {
-    console.log('Socket connected')
-
-    socket.sendObject = (id, type, data) => {
-        console.log('sendObject')
-        // const b = new Blob([slug, data])
-
-        var o = new Uint8Array(data.length + 2);
-        o.set([id, type], 0)
-        o.set(data, 2)
-        socket.send(o);
-    }
-    socket.sentHeaders = new Set()
-
-    Object.getOwnPropertyNames(headers).forEach(id => {
-        sendFTYPMOOV(id, socket)
-    })
-});
-
 function sendFTYPMOOV (id, socket) {
     if (headers[id] && headers[id].ftyp && headers[id].moov) {
         console.log('Websocket: Sending ftyp and moov segments to client.');
@@ -103,51 +90,29 @@ function sendFTYPMOOV (id, socket) {
         ftyp_moov.set(headers[id].ftyp, 0);
         ftyp_moov.set(headers[id].moov, headers[id].ftyp.length);
 
-        socket.sendObject(id, 99, ftyp_moov);
+        sendObject(socket, id, 99, ftyp_moov);
         socket.sentHeaders.add(id)
     }
 }
 
-socketServer.broadcast = function(id, type, data) {
-    socketServer.clients.forEach(function each(client) {
-        
-        if (client.readyState === WebSocket.OPEN) {
+function broadcast (id, type, data) {
+    app.ws.server.clients.forEach(function each(client) {
+        if (client.readyState === WebSocketOpen) {
             if (!client.sentHeaders.has(id)) {
                 sendFTYPMOOV(id, client)
             } 
             
-            client.sendObject(id, type, data);
+            sendObject(client, id, type, data);
         }
     });
 };
 
-// // TCP Server
-// net.createServer(function (tcpSocket) {
-//     // Identify this client
-//     tcpSocket.name = tcpSocket.remoteAddress + ":" + tcpSocket.remotePort;
-
-//     console.log('TCP: Receiving data from ', tcpSocket.name);
-
-//     tcpSocket.on('data', function (data) {
-//         console.log('data received', data.byteLength)
-//         if (!moov) {
-//             console.log('Get ftyp & moov fragments')
-//             initFragment(data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength));
-
-//             socketServer.clients.forEach(function each(client) {
-//                 client.sentFTYP_MOOV = false
-//             })
-//         }
-//         socketServer.broadcast(2, 100, data);
-//     });
-
-//     tcpSocket.on('close', function(code, message){
-//         console.log('Clear ftyp & moov fragments')
-//         ftyp = undefined
-//         moov = undefined;
-//     });
-
-// }).listen(STREAM_PORT);
+function sendObject (socket, id, type, data) {
+    var o = new Uint8Array(data.length + 2);
+    o.set([id, type], 0)
+    o.set(data, 2)
+    socket.send(o);
+}
 
 const ffmpeg = require('fluent-ffmpeg')
 function videoConsumer (camera) {
@@ -164,16 +129,15 @@ function videoConsumer (camera) {
     });
     const ffstream = command.pipe()
     ffstream.on('data', function(data) {
-        console.log('data received', data.byteLength)
             if (!headers[camera] || !headers[camera].moov) {
                 console.log('Get ftyp & moov fragments')
                 initFragment(camera, data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength));
 
-                socketServer.clients.forEach(function each(client) {
+                app.ws.server.clients.forEach(function each(client) {
                     client.sentHeaders.clear()
                 })
             }
-            socketServer.broadcast(camera, 100, data);
+            broadcast(camera, 100, data);
     });
 }
 
@@ -184,7 +148,3 @@ videoConsumer(5)
 videoConsumer(6)
 videoConsumer(7)
 videoConsumer(8)
-
-
-// console.log('Listening for incomming MP4 Stream on tcp://127.0.0.1:' + STREAM_PORT + '/');
-console.log('Awaiting WebSocket connections on ws://127.0.0.1:' + WEBSOCKET_PORT + '/');
