ec63ba071af5e03b03abcf06faac7c3822e8ef9d
Tim Bailey <timb@bailey9.com>
2024-09-01 08:38:30 +1000

    feat: reduce frigate logging

--- notifications/frigate.js
+++ notifications/frigate.js
@@ -1,50 +1,58 @@
-const baselog = require('loglevel').getLogger('notifications:frigate-base')
-const joi = require('joi')
+const baselog = require("loglevel").getLogger("notifications:frigate-base");
+const joi = require("joi");
 
 const schema = joi.object({
-  type: joi.string().lowercase().allow('frigate').required(),
-  camera: joi.string().required()
-})
+  type: joi.string().lowercase().allow("frigate").required(),
+  camera: joi.string().required(),
+});
 
 const factory = () => {
-  baselog.info('Create')
+  baselog.info("Create");
 
-  const decoder = new TextDecoder()
-  const encoder = new TextEncoder()
+  const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
 
   return {
-    start
-  }
+    start,
+  };
 
-  function start (config, sourceConfig, itemConfig, server, mqttClient) {
-    const log = require('loglevel').getLogger('notifications:frigate-' + sourceConfig.id)
-    log.debug('Start')
+  function start(config, sourceConfig, itemConfig, server, mqttClient) {
+    const log = require("loglevel").getLogger(
+      "notifications:frigate-" + sourceConfig.id,
+    );
+    log.debug("Start");
 
     const inheritedConfig = {
       //...sourceConfig,
-      ...itemConfig
-    }
+      ...itemConfig,
+    };
 
-    const { value: notificationConfig, error } = schema.validate(inheritedConfig)
+    const { value: notificationConfig, error } =
+      schema.validate(inheritedConfig);
     if (error) {
-      throw new Error('Invalid configuration for notifications:frigate, ' + error.message)
+      throw new Error(
+        "Invalid configuration for notifications:frigate, " + error.message,
+      );
     }
 
-    log.debug('Loaded config', notificationConfig)
+    log.debug("Loaded config", notificationConfig);
 
-    mqttClient.subscribe('frigate/events', err => {
-      if(err) {
-        log.error('Failed to subscribe to frigate/events', err)
+    mqttClient.subscribe("frigate/events", (err) => {
+      if (err) {
+        log.error("Failed to subscribe to frigate/events", err);
       } else {
-        log.info('Subscribed to frigate/events')
+        log.info("Subscribed to frigate/events");
       }
-    })
+    });
 
-    mqttClient.on('message', (topic, message) => {
-      if (topic.includes('frigate/events')) {
-        const data = JSON.parse(decoder.decode(message))
-        if (data.after.camera === notificationConfig.camera && !data.after.stationary) {
-          log.info('frigate message', data.after)
+    mqttClient.on("message", (topic, message) => {
+      if (topic.includes("frigate/events")) {
+        const data = JSON.parse(decoder.decode(message));
+        if (
+          data.after.camera === notificationConfig.camera &&
+          !data.after.stationary
+        ) {
+          //log.info('frigate message', data.after)
 
           const payload = {
             id: data.after.id,
@@ -53,15 +61,15 @@ const factory = () => {
             endTime: data.after.end_time,
             score: data.after.score,
             box: data.after.box,
-            label: data.after.label
-          }
+            label: data.after.label,
+          };
 
-          const a = encoder.encode(JSON.stringify(payload))
+          const a = encoder.encode(JSON.stringify(payload));
           //log.info('frigate payload', payload, sourceConfig.id, a)frig
-          server.broadcast(sourceConfig.id, 61, a)
+          server.broadcast(sourceConfig.id, 61, a);
         }
       }
-    })
+    });
 
     //setTimeout(() => {
     //  // Dummy test
@@ -77,8 +85,7 @@ const factory = () => {
     //  log.info('Dummy send')
     //  server.broadcast(6, 61, b)
     //}, 15000)
-
   }
-}
+};
 
-module.exports = factory
+module.exports = factory;
